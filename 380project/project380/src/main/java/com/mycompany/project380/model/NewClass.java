d18a7a023ed3f544eed19cfa7a5b47b2662673d9
labouhk <80883507+buhklab@users.noreply.github.com>
2022-04-19 16:32:11 +0800

    Add files via upload

---
+++ 380project/project380/src/main/java/com/mycompany/project380/model/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project380.model;
+
+
+/**
+ *
+ * @author a1742
+ */
+public class NewClass {
+
+    public void greeting() {
+        System.out.println("Hello, World!");
+    }
+}
